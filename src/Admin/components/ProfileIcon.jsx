0e72c41e6d3476ad2b853b317622d72967f7c304
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-26 19:53:01 +0530

    admin

--- src/Admin/components/ProfileIcon.jsx
+++ src/Admin/components/ProfileIcon.jsx
@@ -3,14 +3,10 @@ import User16 from "../../assert/course-02.jpg";
 import { useSelector } from "react-redux";
 
 function ProfileIcon() {
-
   const users = useSelector(state => state.users.users);
-
   const listing = users?.[0] || "";
   const profile = users?.[1] || "";
-
   return (
-
     <div className="user-profile d-flex align-items-center gap-2">
       <img
         src={profile?.profileImage || User16}
